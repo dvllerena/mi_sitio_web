58314ba69802618d53755eb43ee064bb4ea7c01d
dvllerena <dvllerena41@gmail.com>
2025-08-07 08:28:51 -0400

    Descripción clara del cambio

--- mi_sitio_web/urls.py
+++ mi_sitio_web/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('perdidas/', include('perdidas.urls')),
     path('facturacion/', include('facturacion.urls')),
     path('infoperdidas/', include('infoperdidas.urls')),
+    path('planes/', include('planes.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
